63665f4f70ca2ccab617bfe73653cc88e5cb54a9
s1mme <ratboy1337@hotmail.com>
2013-10-07 17:31:30 +0000

    added usermode support

--- kernel/h/types.h
+++ kernel/h/types.h
@@ -24,7 +24,7 @@ struct regs
     unsigned int gs, fs, es, ds;      
     unsigned int  edi, esi, ebp, ebx, edx, ecx, eax;  
     unsigned int int_no, err_code;    
-    unsigned int eip, cs, eflags, useresp; /*,ss*/   
+    unsigned int eip, cs, eflags, useresp,ss;  
 }__attribute__((packed));
 
 typedef struct regs regs_t;
--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -10,10 +10,9 @@
 #include <heapmngr.h>
 #include <kbd.h>
 #include <fat.h>
-
+#include <proc.h>
 void kmain(struct multiboot *mbp, u32 magic)
 {
-	
 	  init_video_term();
 	  gdt_install();
 	  idt_install();
@@ -39,16 +38,17 @@ void kmain(struct multiboot *mbp, u32 magic)
 	  free(d);
 	  scan_pci_bus(0);
 	  ata_init_and_detect_drives();
+	  
 	 /* write_disc_sector();*/ 
 	  /*read_disc_sector(1);*/
-	   mount_fat32();
-	   FAT_testing();
-	   _task_initialize();
-	   TASK_testing();
-	while(1)
-	{
-	  kputch(getchar());
-	}
+	  mount_fat32();
+	  FAT_testing();
+	  _task_initialize();
+	  TASK_testing();
+	  while(1)
+	  {
+		  kputch(getchar());
+	  }
 	/*  u32 *ptr = (u32*)0x600000;
 	  u32 do_page_fault = *ptr;*/
 	  for(;;);
