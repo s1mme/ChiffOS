dc803b0042b6483b244b7021dc4463341629aeaa
s1mme <ratboy1337@hotmail.com>
2013-10-16 20:33:29 +0000

    user test update

--- user/test/test.c
+++ user/test/test.c
@@ -16,17 +16,36 @@ void main()
 	setvbuf(stdin, NULL, _IONBF, 0);		/* important!! */
     setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stderr, NULL, _IONBF, 0);
+	FILE *ptr_file;
+
+    char buf[18];
+
+   /* ptr_file =fopen("znark.txt","r");
+	int i;
+    fgets(buf,7, ptr_file);
+    printf("%s", buf);
+ */
+    ptr_file =fopen("saturn.txt","r");
+	fgets(buf,100, ptr_file);
+    printf("%s", buf);
+        
+    ptr_file = fopen("file", "w"); 
     
-	
-   FILE * fp;
+    FILE * fp;
 
-   fp = fopen ("kosmisk", "w");
-   fprintf(fp, "%s %s %s %d", "We", "are", "in", 2013);
+    fp = fopen ("kosmisk", "w");
+    fprintf(fp, "%s %s %s %d", "We", "are", "in", 2013);
+  
    
-   fclose(fp);
-   int num = 1234;
-   printf("number : %d", num);
-
+    fclose(fp);
+    int num = 1234;
+    printf("number : %d", num);   
+    
+    ptr_file =fopen("text.txt","r");
+	char ch;
+ 
+    while( ( ch = fgetc(ptr_file) ) != EOF )
+		printf("%c",ch);
 
    for(;;);
 }
